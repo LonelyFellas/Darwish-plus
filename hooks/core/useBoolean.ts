deaa6e2ab2f99c96b4350033a4e967ce95253650
worksapce <289582304@qq.com>
2024-03-19 22:12:32 +0800

    refactor: useboolean refactor

--- hooks/core/useBoolean.ts
+++ hooks/core/useBoolean.ts
@@ -1,28 +1,47 @@
-import React from "react";
-export type UseBooleanFunc = (
-  settingValue: boolean | React.MouseEvent<HTMLButtonElement, MouseEvent>
-) => void;
+import React, { ChangeEvent, useMemo } from "react";
+export type UseBooleanOutput = {
+  value: boolean;
+  setTrue: () => void;
+  setFalse: () => void;
+  toggle: (settingValue: ChangeEvent<any> | boolean) => void;
+};
 /**
  * @author darwish
- * @param boolVal a boolean value
- * @description A hook to toggle boolean value
- * @returns [boolean, (settingValue: boolean) => void] -> Array
+ * @param defaultValue a boolean value
+ * @description a hook to toggle boolean value
+ * @returns `UseBooleanOutput` An object containing the boolean state value and utility functions to manipulate the state.
+ * @property {Function} `UseBooleanOutput.value` the current boolean value
+ * @property {Function} `UseBooleanOutput.setTrue` sets the value to `true`
+ * @property {Function} `UseBooleanOutput.setFalse` sets the value to `false`
+ * @property {Function} `UseBooleanOutput.toggle` toggles the value
+ * @see [Documentation](https://darwish.vercel.app/hooks/use-boolean)
  * @example
- * const [state, toggle] = useBoolean(true);
+ * const {value, setTrue, setFalse, toggle} = useBoolean(true);
  * <button onClick={toggle}>Toggle</button>
  */
-const useBoolean: (boolVal: boolean) => [boolean, UseBooleanFunc] = (
-  defaultValue: boolean
+const useBoolean: (defaultValue?: boolean) => UseBooleanOutput = (
+  defaultValue
 ) => {
-  const [state, setState] = React.useState(defaultValue);
+  const [value, setValue] = React.useState(!!defaultValue);
 
-  const toggle = (
-    settingValue: boolean | React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    setState((prev) =>
-      typeof settingValue === "boolean" ? settingValue : !prev
-    );
+  const utils = useMemo(() => {
+    const setTrue = () => setValue(true);
+    const setFalse = () => setValue(false);
+    const toggle = (settingValue: boolean | ChangeEvent<any>) => {
+      setValue((prev) =>
+        typeof settingValue === "boolean" ? settingValue : !prev
+      );
+    };
+    return {
+      setTrue,
+      setFalse,
+      toggle,
+    };
+  }, [value]);
+
+  return {
+    value,
+    ...utils,
   };
-  return [state, toggle];
 };
 export default useBoolean;
