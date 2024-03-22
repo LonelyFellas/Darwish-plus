145ff025f699a3dee2f2b5cf6da5855d16ef35b2
worksapce <289582304@qq.com>
2024-03-22 17:48:53 +0800

    feat: refactor switch component

--- ui/only-when/src/switch.tsx
+++ ui/only-when/src/switch.tsx
@@ -1,4 +1,3 @@
-import { createElement, Fragment } from "react";
 import type { PropsWithChildren } from "react";
 import { isObject, isArray } from "@darwish/utils-is"
 import If from "./if";
@@ -9,30 +8,21 @@ type LabelProps<T extends Label> = {
   as: T;
   tagProps?: TagProps<T>;
 };
-export function Switch(props: PropsWithChildren<{}>): JSX.Element;
-export function Switch<T extends Label>(props: PropsWithChildren<LabelProps<T>>): JSX.Element;
-export function Switch<T extends Label = any>(
-  props: PropsWithChildren<{
-    as?: T;
-    tagProps?: TagProps<T>;
-  }>
+export function Switch(
+  props: PropsWithChildren<{}>
 ) {
-  const { as: Component, tagProps, children } = props;
-  const isCmpUndef = Component === undefined;
+  const { children } = props;
 
   let filterChildren = undefined;
   if (isArray(children)) {
-    filterChildren = children.filter((child) => {
-      return child.type === Case || child.type === Default;
-    })
-  } else if (isObject(children) && ((children as any).type === Case || (children as any).type === Default)){
+    filterChildren = children.filter((child) => child.type === Case || child.type === Default)
+  } else if (isObject(children) && ((children as any).type === Case || (children as any).type === Default)) {
     filterChildren = [children];
   } else {
     console.error("Switch children must be an array of Case or Default components");
   }
 
-
-  return createElement(Component || Fragment, isCmpUndef ? {} : tagProps, filterChildren);
+  return <>{filterChildren}</>
 }
 
 interface CaseLabelProps<T extends Label> {
@@ -40,14 +30,16 @@ interface CaseLabelProps<T extends Label> {
   as: T;
   tagProps?: TagProps<T>;
 }
-export function Case(props: PropsWithChildren<{is?: boolean}>): JSX.Element;
+export function Case(props: PropsWithChildren<{ is?: boolean }>): JSX.Element;
 export function Case<T extends Label>(props: PropsWithChildren<CaseLabelProps<T>>): JSX.Element;
-export function Case<T extends Label>(props: PropsWithChildren<{is?: boolean, as?: T, tagProps?: JSX.IntrinsicElements[T]}>) {
-  const { is = true, as: Component, tagProps } = props;
+export function Case<T extends Label>(props: PropsWithChildren<{ is?: boolean, as?: T, tagProps?: JSX.IntrinsicElements[T] }>) {
+  const { is = true, as: Component, tagProps, children } = props;
   const isCmpUndef = Component === undefined;
 
+  // @ts-ignore
+  const renderedJSX = isCmpUndef ? <>{children}</> : <Component {...tagProps}>{children}</Component>
   return (
-    <If is={is} render={createElement(Component|| Fragment, isCmpUndef ? {} : tagProps, props.children)} />
+    <If is={is} render={renderedJSX} />
   );
 }
 
@@ -59,8 +51,11 @@ export function Default<T extends Label>(
     tagProps?: TagProps<T>;
   }>
 ) {
-  const { as: Component, tagProps} = props;
+  const { as: Component, tagProps } = props;
   const isCmpUndef = Component === undefined;
 
-  return createElement(Component || Fragment, isCmpUndef ? {} : tagProps, props.children);
+  // @ts-ignore
+  return isCmpUndef ? <>{children}</> : <Component {...tagProps}>{children}</Component>
+
+  // return createElement(Component || Fragment, isCmpUndef ? {} : tagProps, props.children);
 }
